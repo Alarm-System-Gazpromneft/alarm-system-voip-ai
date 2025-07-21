ead58af99e2c6119d96e03c659150d59981c2902
Khokhlov Kirill <hkp69@yandex.ru>
2025-07-21 23:11:01 +0300

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -13,5 +13,5 @@ sleep 2
 pactl load-module module-null-sink sink_name=virtual_sorc sink_properties=device.description="Virtual_sorc"
 pactl set-default-source virtual_sorc.monitor
 # paplay '/app/song.wav' --device=virtual_sorc
-python3 websok.py & python3 _vosk_loop.py --model /app/vosk-model-ru-0.22 -d 1
+python3 websok.py & python3 _vosk_loop.py --model /app/vosk-model-ru-0.42 -d 1
 # sleep 10000
